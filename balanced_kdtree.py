03e0f628dd50b1aa48c161e5037458b0ec17c443
Ash <ash975@live.com>
2017-03-03 15:21:37 +0800

    Add files via upload

--- balanced_kdtree.py
+++ balanced_kdtree.py
@@ -1,42 +1,40 @@
-class Node():
-    def __init__(self, point, dimention):
-        self.right = []
-        self.left = []
+class Node:
+    def __init__(self, point, dimension, depth, left=[], right=[]):
+        self.left = left
+        self.right = right
         self.point = point
-        self.dimention = dimention
+        self.dimension = dimension
+        self.depth = depth
+
 
 def median(data):
-    try:
-        m = int(len(data) / 2)
-        return data[m], m
-    except Exception:
-        pass
+    m = int(len(data) / 2)
+    return data[m], m
 
-def build_tree(data, dimention, count, node_list):
-    try:
-        data = sorted(data, key = lambda x: x[dimention])
+
+def build_tree(data, dimension, node_list, k, depth=0):
+    if depth < k:
+        data = sorted(data, key=lambda x: x[dimension])
         data_median, index_medina = median(data)
         del data[index_medina]
-        node_list.append(Node(data_median,dimention))
-        node_list[count].left = data[:index_medina]
-        node_list[count].right = data[index_medina:]
-        dimention = not dimention
-        count += 1
-        if index_medina > 0 :build_tree(data[:index_medina],dimention, count, node_list)
-        if len(data) > 1 :build_tree(data[index_medina:], dimention, count, node_list)
-        return node_list
-    except Exception:
-        pass
+        node_list.append(Node(data_median, dimension, depth, left=data[:index_medina],
+                              right=data[index_medina:]))
+        depth += 1
+        if index_medina > 0: build_tree(data[:index_medina], not dimension, node_list, k, depth=depth)
+        if len(data) > 1: build_tree(data[index_medina:], not dimension, node_list, k, depth=depth)
+    return node_list
+
 
 def train(data):
     node_list = []
-    dimention = False
-    count = 0
-    node_list = build_tree(data, dimention, count, node_list)
+    dimension = False
+    node_list = build_tree(data, dimension, node_list, k=3)
 
     for i in range(len(node_list)):
-        print(node_list[i].point, node_list[i].dimention)
+        print("node:", i, "\tpoint:", node_list[i].point, "\tdepth:", node_list[i].depth,
+              "\tdimension:", node_list[i].dimension, "\tleft:", node_list[i].left,
+              "right:", node_list[i].right)
+
 
 T = [[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]]
-
 train(T)
